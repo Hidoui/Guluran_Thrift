ea18c60b99fb6bdce7013613f4ec52a50e4a5157
Hido <hidoujin@gmail.com>
2024-11-29 12:39:09 +0700

    Rename Database

--- Guluran/db.php
+++ Guluran/db.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "db_guluran";
+$dbname = "guluran_thrift";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
